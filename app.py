688d07dabe0062ed660643234c58643cd1b10ec1
Paras Jain <parasjain08803@gmail.com>
2025-10-15 11:37:29 +0530

    update app.py

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ documents = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 docs = text_splitter.split_documents(documents)
 
-embeddings = HuggingFaceEmbeddings()
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"device": "cpu"}
+)
 vectorstore = FAISS.from_documents(docs, embeddings)
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
